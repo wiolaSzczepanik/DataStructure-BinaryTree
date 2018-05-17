872f09c4c595fef2ae3adc5ca6046a2ce5d4f1c9
wiolaSzczepanik <wiolaszcz@gmail.com>
2018-05-17 15:18:36 +0200

    implement method insert

--- src/main/java/BinaryTree.java
+++ src/main/java/BinaryTree.java
@@ -1,3 +1,25 @@
 public class BinaryTree {
     Node root;
+
+    public BinaryTree(Node root) {
+        this.root = root;
+    }
+
+    public void insert(int value){
+        if(value<root.value){
+            if(root.left!=null){
+                insert(value);
+            } else{
+                System.out.println("Inserted "+ value + " to left of " + root.value);
+                root.left = new Node(value);
+            }
+        }if(value>root.value){
+            if(root.right !=null){
+                insert(value);
+            } else{
+                System.out.println("Inserted "+ value + " to left of " + root.value);
+                root.right = new Node(value);
+            }
+        }
+    }
 }
